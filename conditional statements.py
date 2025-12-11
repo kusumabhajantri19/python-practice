ea5863c89c52b95ab50b2e37b31c1673b8df7bd9
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2025-12-11 20:36:43 +0530

    first commit

---
+++ conditional statements.py
@@ -0,0 +1,78 @@
+num = int(input("enter a number"))
+if num  == 0:
+    print("zero")
+elif num % 2 == 0:
+    print("positive")
+else:
+    print("negative")
+
+#
+# num = int(input("enter a number"))
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+
+# num = int(input("enter a number"))
+# if num % 5 == 0:
+#     print("divisible by 5")
+# else:
+#     print("not divisible by 5")
+
+
+#
+# num = int(input("enter a number"))
+# if num % 5 == 0 and num % 3 == 0:
+#     print("divisible ")
+# else:
+#     print("not divisible ")
+
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a number: "))
+#
+# if num1 > num2:
+#     print(f"{num1} is larger than {num2}")
+# elif num2 > num1:
+#     print(f"{num2} is larger than {num1}")
+# else:
+#     print("Both numbers are equal")
+
+#
+# num1 = int(input("Enter a number: "))
+# num2 = int(input("Enter a number: "))
+# num3 = int(input("Enter a number: "))
+#
+# if num1 >= num2 and num1 >= num3:
+#     print(f"{num1} is the largest")
+# elif num2 >= num1 and num2 >= num3:
+#     print(f"{num2} is the largest")
+# else:
+#     print(f"{num3} is the largest")
+#
+# num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+# num3 = int(input("Enter number 3: "))
+#
+# largest = max(num1, num2, num3)
+#
+# print("Largest number is:", largest)
+#
+#
+# temperature = int(input ("enter temperature: "))
+# temp = 270
+# if temp > temperature:
+#     print("cold")
+# elif temp < temperature:
+#     print("heat")
+# else:
+#     print("normal")
+
+
+
+
+
+
+
+
+
