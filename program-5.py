2453919f182f8a0fed4a90e0282f59b12422c868
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2025-12-13 23:14:32 +0530

    Add Program 5

---
+++ program-5.py
@@ -0,0 +1,9 @@
+
+# temperature = int(input ("enter temperature: "))
+# temp = 270
+# if temp > temperature:
+#     print("cold")
+# elif temp < temperature:
+#     print("heat")
+# else:
+#     print("normal")
