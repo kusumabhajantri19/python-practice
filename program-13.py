3e6675f308b32a093bd6ded0434d77d090b26356
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2025-12-16 21:40:41 +0530

    Add Program-13

---
+++ program-13.py
@@ -0,0 +1,11 @@
+
+
+hours = int(input("Enter a hours(0-24):"))
+if hours >=5 and hours <12:
+    print("Good morning")
+elif hours>= 12 and hours <17:
+    print("Good afternoon")
+elif hours >=17 and hours <21:
+    print("Good evening")
+else:
+    print("Good night")
