c5373919eb1f67e885d0c09246141e2680309b7e
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2025-12-20 23:12:40 +0530

    Add Program-31

---
+++ program-31.py
@@ -0,0 +1,12 @@
+
+
+password = input("Enter a password: ")
+
+if len(password) >= 8 and (
+    '0' in password or '1' in password or '2' in password or '3' in password or
+    '4' in password or '5' in password or '6' in password or '7' in password or
+    '8' in password or '9' in password
+):
+    print("Correct password")
+else:
+    print("Incorrect password")
