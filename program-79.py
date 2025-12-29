8c2f5a50616439de4888246a0e8a0624d9c4fe90
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2025-12-29 22:51:20 +0530

    Add Program-79

---
+++ program-79.py
@@ -0,0 +1,15 @@
+
+
+# Print only odd numbers from 1 to n recursively.
+
+def print_even(n):
+    if n == 0:
+        return
+
+    print_even(n - 1)
+
+    if n % 2 != 0:
+        print(n)
+
+n = int(input("Enter a number: "))
+print_even(n)
