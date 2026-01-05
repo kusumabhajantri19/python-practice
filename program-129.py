c2e8af05e3d872bab420de5ce44ab97a4757118a
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-05 18:41:35 +0530

    Add Program-129

---
+++ program-129.py
@@ -0,0 +1,11 @@
+
+#  Print the first and last character of a string.
+
+
+n = input("Enter a name: ")
+
+first = n[0]          
+last = n[-1]          
+
+print("First character:", first)
+print("Last character:", last)
