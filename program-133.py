c4616e6a680db90ec255c0e415dd041adc985635
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-05 18:49:30 +0530

    Add Program-133

---
+++ program-133.py
@@ -0,0 +1,11 @@
+
+# Take two strings and print them concatenated.
+
+n1 = input("Enter a string:")
+n2 = input("Enter a string:")
+n3 = n1 + n2
+count = 0
+for i in n3:
+    count +=1
+print(n3)
+print(count)
