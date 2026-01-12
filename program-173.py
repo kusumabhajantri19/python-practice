51cd6633219facdc90fab7bb83c5cc7f2e27e58d
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-12 23:41:08 +0530

    Add Program-173

---
+++ program-173.py
@@ -0,0 +1,13 @@
+
+word = "khushi"
+result = ""
+
+for ch in word:
+    if ch == "k":
+        result += "i"
+    elif ch == "i":
+        result += "k"
+    else:
+        result += ch
+
+print(result)
