3f519bee74381c365e2fdfec17ffae1689914524
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-15 20:57:00 +0530

    Add Program-194

---
+++ program-194.py
@@ -0,0 +1,10 @@
+
+def number(a,b):
+    if a > b:
+        print(f"{a}  is greater than number {b}")
+    else:
+        print(f"number {a} is less than number {b}")
+# number(3,2)
+num1 = int(input("Enter a number:"))
+num2 = int(input("Enter a number:"))
+number(num1,num2)
