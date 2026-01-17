50ea060114874c56687d7559b7f65bb9b2291cce
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-18 00:06:16 +0530

    Add Program-208

---
+++ program-208.py
@@ -0,0 +1,9 @@
+
+seat1 = int (input("Enter a seat number:"))
+seat = 8
+while seat <= 8:
+    if seat == 0:
+         seat -=1
+         print("no seat")
+         break
+print("All seats are booked"
