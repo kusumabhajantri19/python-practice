13d4803176d84dc8751ae48a6f79cb094f9001d0
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-20 00:06:45 +0530

    Add Program-222

---
+++ program-222.py
@@ -0,0 +1,11 @@
+
+#Default value
+
+def marriage(boy,girl="Girl"):
+    print(f"Boy is {boy}")
+    print(f"Girl is {girl}")
+    print(f"{boy} married {girl}")
+
+marriage("khushi")
+marriage("hi")
+marriage(boy="hi")
