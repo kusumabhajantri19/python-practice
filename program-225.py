31a077a461ea004444f4bc999ad0ab886f32cf50
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-20 00:11:53 +0530

    Add Program-225

---
+++ program-225.py
@@ -0,0 +1,12 @@
+
+#Local variable And Global variable
+
+def func():
+    x = "khushi"
+    print("Hello World")
+    print(y)       #Global variable prints
+    print(x)
+
+y = "hi"
+print(x)
+func()     #local variable prints
