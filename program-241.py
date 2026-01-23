2044412989bd1f5ae967d213e8c6f41d8287fe0c
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-23 23:38:14 +0530

    Add Program-241

---
+++ program-241.py
@@ -0,0 +1,12 @@
+
+def login_function_call(func):
+    def wrapper():
+        print(f"function called {func.__name__}..")
+        func()
+    return wrapper
+
+@login_function_call
+def add():
+    print("Ravi profile opened")
+
+add()
