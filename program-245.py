91ede16daeb4a643f8f1a4c214606bf188fcd29c
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-24 23:17:41 +0530

    Add Program-245

---
+++ program-245.py
@@ -0,0 +1,11 @@
+
+try:
+    num = int(input("Enter a number:"))
+    result = 10/num
+    print("Result:",result)
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+except ValueError:
+    print("Please enter a valid number")
+except Exception :
+    print("error")
