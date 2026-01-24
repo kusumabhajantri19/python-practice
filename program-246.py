ebb32c0ce6ce46d8059ea1cf1859e5954e99b1fd
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-24 23:18:31 +0530

    Add Program-246

---
+++ program-246.py
@@ -0,0 +1,9 @@
+
+try:
+    boy = input("Who do you friend..")
+    if boy.lower() != "khushi":
+        raise Exception("You can only friend khushi.select her!")
+except Exception as e:
+    print(f"Error:{e}")
+else:
+    print("yes . khushi is ready")
