26048d5c455b4941acfd6e51215768e12784726e
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-27 02:43:53 +0530

    Add code to concat_strings program

--- strings/concat_strings.py
+++ strings/concat_strings.py
@@ -1,11 +1,10 @@
 
-# Take two strings and print them concatenated.
+# Program to concatenate two strings and count characters
 
-n1 = input("Enter a string:")
-n2 = input("Enter a string:")
-n3 = n1 + n2
-count = 0
-for i in n3:
-    count +=1
-print(n3)
-print(count)
+n1 = input("Enter first string: ")
+n2 = input("Enter second string: ")
+
+result = n1 + n2
+
+print("Concatenated string:", result)
+print("Total characters:", len(result))
