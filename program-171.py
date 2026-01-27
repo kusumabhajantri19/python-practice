c574a882c61ab6b40f6bb0119692c83b6761df6f
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-27 22:49:16 +0530

    Remove old numbered string programs

--- program-171.py
+++
@@ -1,8 +0,0 @@
-
-#print the sentence in title case (first letter capital, rest lowercase.
-
-sentence = input("Enter a sentence: ")
-
-capitalized_sentence = sentence.title()
-
-print("Sentence with first letter of each word capitalized:", capitalized_sentence)
--- program-208.py
+++
@@ -1,9 +0,0 @@
-
-seat1 = int (input("Enter a seat number:"))
-seat = 8
-while seat <= 8:
-    if seat == 0:
-         seat -=1
-         print("no seat")
-         break
-print("All seats are booked"
