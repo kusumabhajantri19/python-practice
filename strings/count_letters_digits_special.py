2992d258b7cc7894b19f4a00c631ad97d2899dd9
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-27 15:23:17 +0530

    Add 5 string programs: vowels/consonants, letters/digits/special, uppercase/lowercase, spaces

---
+++ strings/count_letters_digits_special.py
@@ -0,0 +1,22 @@
+
+# Count the number of letters, digits, and special characters in a string
+
+s = input("Enter a string: ")
+
+digits = 0
+letters = 0
+special = 0
+
+for ch in s:
+    ascii_value = ord(ch)  
+
+    if 48 <= ascii_value <= 57:  
+        digits += 1
+    elif (65 <= ascii_value <= 90) or (97 <= ascii_value <= 122): 
+        letters += 1
+    else:
+        special += 1  
+
+print("Letters:", letters)
+print("Digits:", digits)
+print("Special characters:", special)
---
+++ strings/count_uppercase_lowercase.py
@@ -0,0 +1,16 @@
+
+# Count how many uppercase and lowercase letters a string has.
+
+s = input("Enter a string: ")
+
+upper = 0
+lower = 0
+
+for ch in s:
+    if 'A' <= ch <= 'Z':   
+        upper += 1
+    elif 'a' <= ch <= 'z': 
+        lower += 1
+
+print("Uppercase letters:", upper)
+print("Lowercase letters:", lower)
