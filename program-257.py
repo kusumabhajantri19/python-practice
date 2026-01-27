bd25f3693db5e95de7f349665d03c7d5b27f3a78
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-27 21:35:49 +0530

    Add Program-257

---
+++ program-257.py
@@ -0,0 +1,10 @@
+
+#3- space right side right angle triangle
+
+for i in range(5):
+    for j in range(i,5):
+        print(" ", end="")
+    for i in range(i+1):
+
+        print("*", end="")
+    print()
