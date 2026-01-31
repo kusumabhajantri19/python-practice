49f0e0828fef494e93e79d6c0d20ac1057fb2d1a
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-01-31 22:46:56 +0530

    Delete old program files

--- program-246.py
+++
@@ -1,9 +0,0 @@
-
-try:
-    boy = input("Who do you friend..")
-    if boy.lower() != "khushi":
-        raise Exception("You can only friend khushi.select her!")
-except Exception as e:
-    print(f"Error:{e}")
-else:
-    print("yes . khushi is ready")
