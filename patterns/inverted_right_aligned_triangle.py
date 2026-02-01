a8386489d42c7b1b806d354c71cfe08f67ff7a97
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-02-01 23:41:19 +0530

    Add patterns folder

---
+++ patterns/inverted_right_aligned_triangle.py
@@ -0,0 +1,10 @@
+
+# Inverted Right-Aligned Right Angle Triangle Star Pattern
+
+for i in range(5):
+    for j in range(i+1):
+        print(" ", end="")
+    for j in range(i,5):
+
+        print("*", end="")
+    print()
---
+++ patterns/inverted_right_angle_triangle.py
@@ -0,0 +1,7 @@
+
+# Inverted Right Angle Triangle Star Pattern
+
+for i in range(5):
+    for j in range(5-i):
+        print("*", end = "")
+    print()
