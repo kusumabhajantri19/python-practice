484f8acf79c7c0a8566f2b5b38a7b089d2ebccfb
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-02-01 23:19:04 +0530

    Add patterns folder

---
+++ patterns/reverse_number_right_angle_triangle.py
@@ -0,0 +1,9 @@
+
+
+#Reverse Number right angle Triangle Pattern
+
+for i in range(1, 6):
+    for j in range(1,6-i+1):
+        print(j, end=" ")
+
+    print()
---
+++ patterns/square_star_pattern.py
@@ -0,0 +1,7 @@
+
+# Square Star Pattern
+
+for i in range(5):
+    for j in range(5):
+        print("*", end = " ")
+    print()
