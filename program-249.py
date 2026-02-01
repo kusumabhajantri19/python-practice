eb5140a8546cb18045ee67ed218dd1ad8c741e28
kusumabhajantri19 <kusumabhajantri350@gmail.com>
2026-02-01 23:05:09 +0530

    add programs

--- program-249.py
+++
@@ -1,9 +0,0 @@
-
-
-#pattern
-
-nums =[1,2,3,4,5]
-for i in range(len(nums)):
-    for j in range(5-i):
-        print(nums[j], end = " ")
-    print("\n")
--- program-251.py
+++
@@ -1,9 +0,0 @@
-
-
-# pattern
-num = 1
-for i in range(1, 6):
-    for j in range(i):
-        print(num, end=" ")
-        num += 1
-    print()
