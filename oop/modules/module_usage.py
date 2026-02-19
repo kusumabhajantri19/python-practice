497162eb4167514889fed9ba9ac3625bd908f1fb
Kusuma Bhajantri <kusumabhajantri350@gmail.com>
2026-02-19 10:27:30 +0530

    Add module_usage.py demonstrating import of custom module

---
+++ oop/modules/module_usage.py
@@ -0,0 +1,14 @@
+
+
+#Importing and Using a Custom Module in Python
+
+from my_module import a, area_of_square, calculator
+a,b=4,5
+sum=a+b
+print("sum is : ", sum)
+print("Value of a from another module is:", a)
+print("Area of square:", area_of_square(4))
+
+calculator(3, 2)
+
+
