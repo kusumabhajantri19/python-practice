75954a94818c488320355ec7ea255abf7ce03678
Kusuma Bhajantri <kusumabhajantri350@gmail.com>
2026-02-19 10:23:14 +0530

    Add math_module.py demonstrating custom module concept

---
+++ oop/modules/math_module.py
@@ -0,0 +1,13 @@
+
+
+#Creating and Using a Custom Module in Python
+
+a = 10
+def area_of_square(side):
+    return side ** 2
+
+def calculator(x,y):
+    print("Addition is: ", x+y)
+    print("Substraction is: ", x-y)
+    print("Multiplication is: ", x*y)
+    print("Division is: ", x/y)
