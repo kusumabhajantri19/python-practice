c8cedefbfe06fee2104dd4130ec5b5e102f8104a
Kusuma Bhajantri <kusumabhajantri350@gmail.com>
2026-02-21 21:24:50 +0530

    Add complex_operator_overloading.py demonstrating operator overloading using __add__

---
+++ oop/operator_overloading/complex_operator_overloading.py
@@ -0,0 +1,14 @@
+
+
+#Operator Overloading using Magic Method (add) in Python
+
+class ComplexNumber:
+    def __init__(self,r,i):
+        self.real=r
+        self.imaginary=i
+    def __add__(self,other):
+        return f"{self.real + other.real} + {self.imaginary+other.imaginary}i "
+        # return str(self.real+other.real) + str(self.imaginary+other.imaginary) + "i"
+c1=ComplexNumber(1,2)
+c2=ComplexNumber(3,4)
+print(c1+c2)
