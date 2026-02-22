cfe9ebf6c665e852f3c9b9b48b3b260edb08bdfd
Kusuma Bhajantri <kusumabhajantri350@gmail.com>
2026-02-22 11:56:41 +0530

    Add abstraction_shape.py demonstrating abstraction with Shape class

---
+++ oop/abstraction/abstraction_shape.py
@@ -0,0 +1,19 @@
+
+#Demonstrate abstraction using abstract class for shapes
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+s = Square(5)
+print("Square area:", s.area())
